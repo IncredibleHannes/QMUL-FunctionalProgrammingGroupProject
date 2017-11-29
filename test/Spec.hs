864ea2cff021eee3e9648c8ddd785934969a96dc
Johannes Hartmann <johannes.Hartmann.Calw@web.de>
2017-11-29 16:44:15 +0000

    added some test setup

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,26 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Test.HUnit
+
+main :: IO Counts
+main = do
+  _ <- runTestTT dataBaseModuleTests
+  _ <- runTestTT dataStructuresTests
+  _ <- runTestTT httpRequestModuleTests
+  _ <- runTestTT httpRequestModule2Tests
+  _ <- runTestTT ioActionModuleTests
+  runTestTT jsonParserModuleTests
+
+test1 :: Test
+test1 = TestCase (assertEqual "one equals one" 1 1)
+
+dataBaseModuleTests :: Test
+dataBaseModuleTests     = TestList [TestLabel "Easy Test" test1]
+dataStructuresTests :: Test
+dataStructuresTests     = TestList []
+httpRequestModuleTests :: Test
+httpRequestModuleTests  = TestList []
+httpRequestModule2Tests :: Test
+httpRequestModule2Tests = TestList []
+ioActionModuleTests :: Test
+ioActionModuleTests     = TestList 
+jsonParserModuleTests :: Test
+jsonParserModuleTests   = TestList []
