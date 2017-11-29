1044bfba93f2bb5bb682a1a17b485655cad86fa0
Johannes Hartmann <johannes.Hartmann.Calw@web.de>
2017-11-29 23:36:30 +0000

    added some tests testing the data base module

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,8 @@
 import Test.HUnit
 
+import DataBaseModule
+import DataStructures
+
 main :: IO Counts
 main = do
   _ <- runTestTT dataBaseModuleTests
@@ -9,11 +12,45 @@ main = do
   _ <- runTestTT ioActionModuleTests
   runTestTT jsonParserModuleTests
 
-test1 :: Test
-test1 = TestCase (assertEqual "one equals one" 1 1)
 
 dataBaseModuleTests :: Test
-dataBaseModuleTests     = TestList [TestLabel "Easy Test" test1]
+dataBaseModuleTests = TestList [ TestLabel "Testing if the result is nothing" dataBaseTest,
+                                 TestLabel "Testing the database module" dataBaseTest2,
+                                 TestLabel "More complex test of the database module" dataBaseTest3,
+                                 TestLabel "Trying a inconsistent database" dataBaseTest4,
+                                 TestLabel "Trying a inconsistent database" dataBaseTest5 ]
+
+setupTest :: [Movie] -> [Actor] -> String -> String -> Maybe[Movie] -> Test
+setupTest movie actor name description expected = TestCase ( do
+  conn <- dbConnect
+  _ <- initialiseDB conn
+  _ <- insertMovieIntoDB conn movie
+  _ <- insertActorIntoDB conn actor
+  movie <- searchMoviesInDB conn name
+  disconnectDB conn
+  assertEqual description movie expected
+  )
+
+dataBaseTest :: Test
+dataBaseTest = setupTest [] [] "test" "Shouldn fine a movie" Nothing
+
+
+dataBaseTest2 :: Test
+dataBaseTest2 = setupTest [Movie 1 "Movie"] [Actor 1 "Johannes" [1]]
+                  "Johannes" "Should find a movie" (Just [Movie 1 "Movie"])
+
+dataBaseTest3 :: Test
+dataBaseTest3 = setupTest [ Movie 1 "Doctor Who", Movie 2 "Lord of the Rings", Movie 3 "Star Wars" ]
+                  [ Actor 1 "Johannes" [1,2,3], Actor 2 "Manuel" [2,3], Actor 3 "Liam" [1] ]
+                  "Johannes" "Should find tree a movies"
+                  (Just [ Movie 1 "Doctor Who", Movie 2 "Lord of the Rings", Movie 3 "Star Wars" ])
+
+dataBaseTest4 :: Test
+dataBaseTest4 = setupTest [] [Actor 1 "Clara Oswald" [1]] "Clara Oswald" "Should return Nothig" Nothing
+
+dataBaseTest5 :: Test
+dataBaseTest5 = setupTest [Movie 1 "Doctor Who"] [] "Clara Oswald" "Should return Nothig" Nothing
+
 dataStructuresTests :: Test
 dataStructuresTests     = TestList []
 httpRequestModuleTests :: Test
@@ -21,6 +58,6 @@ httpRequestModuleTests  = TestList []
 httpRequestModule2Tests :: Test
 httpRequestModule2Tests = TestList []
 ioActionModuleTests :: Test
-ioActionModuleTests     = TestList 
+ioActionModuleTests     = TestList []
 jsonParserModuleTests :: Test
 jsonParserModuleTests   = TestList []
