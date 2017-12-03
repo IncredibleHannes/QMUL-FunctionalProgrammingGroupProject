225fb5eeb10c5bc6b93160728a153a310d7ad4c8
liamsscreenname <liamglkelly@gmail.com>
2017-12-03 00:36:24 +0000

    dynamic dates

--- src/HTTPRequestModule.hs
+++ src/HTTPRequestModule.hs
@@ -12,11 +12,41 @@ import JSONParserModule
 import Network.URI
 import Network.HTTP.Simple
 import Network.HTTP.Client
+import Data.DateTime
 import Data.Maybe
 import Data.Either
 
-movieReqURL :: String
-movieReqURL = "https://api.themoviedb.org/3/discover/movie?api_key=77a5749742a2117c0b9c739d7bad6518&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=2016-11-26&primary_release_date.lte=2017-11-26"
+getYr :: (Integer, Int, Int, Int, Int, Int) -> Integer
+getYr (yr, _, _, _, _, _) = yr
+
+getMth :: (Integer, Int, Int, Int, Int, Int) -> Int
+getMth (_, mth, _, _, _, _) = mth
+
+getDay :: (Integer, Int, Int, Int, Int, Int) -> Int
+getDay (_, _, day, _, _, _) = day
+
+yrMinusThree :: Integer -> Integer
+yrMinusThree x = if x <4 then x - 1 else x
+
+mthMinusThree :: Int -> Int
+mthMinusThree x 
+  | x==1 = 10
+  | x==2 = 11
+  | x==3 = 12
+  | x>=4 = x - 3
+
+movieReqURL :: IO String
+movieReqURL = do
+  date' <- getCurrentTime
+  let date = toGregorian date'
+  let gteYr = yrMinus $ getYr date
+  let gteMth = mthMinusThree $ getMth date
+  let day = getDay date
+  let mth = getMth date
+  let yr = getYr date
+  return $ concat ["https://api.themoviedb.org/3/discover/movie?api_key=77a5749742a2117c0b9c739d7bad6518&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=", show gteYr, "-" , show gteMth, "-", show day, "&primary_release_date.lte=", show yr, "-", show mth, "-", show day]
+    where yrMinus x = if x<4 then x-1 else x
+
 
 actorReqURL :: String
 actorReqURL = undefined
@@ -33,7 +63,8 @@ download str = do
 
 httpGetListOfMovies :: IO [Movie]
 httpGetListOfMovies = do
-  moviesStr <- download movieReqURL
+  url <- movieReqURL
+  moviesStr <- download url
   let movies = parseMovies moviesStr
   return movies
 
