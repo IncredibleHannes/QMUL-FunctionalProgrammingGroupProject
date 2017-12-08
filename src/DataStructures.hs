b6f2c3836294ee531e74ed730ff15c3cee29eda7
Johannes Hartmann <johannes.Hartmann.Calw@web.de>
2017-12-08 12:15:06 +0000

    added some code formatting

--- src/DataStructures.hs
+++ src/DataStructures.hs
@@ -19,7 +19,6 @@ module DataStructures(
       CinemaList(CinemaList)
     ) where
 
-
 {-
    | Data structure representing a List of movies
 -}
@@ -29,11 +28,12 @@ newtype MovieList = MovieList {movieList :: [Movie]} deriving Show
      second is the name of the movie and the third the release day -}
 data Movie = Movie Int String String
   deriving (Eq, Show)
+
 {- | Data structure representig a actor. The first parameter is the ID, the
      second is the name of the actor and the third are all movies he plays in -}
-     -- maybe use list of mio
 data Actor = Actor Int String [Movie]
   deriving (Eq, Show)
+
 {- | Data structure representig a cinema. The first parameter is the ID, the
      second is the name of the cinema and the third is the range to the given
      location -}
